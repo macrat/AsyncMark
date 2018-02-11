3a6b68d028ae1b06a736ac25dd06c7124bcacc4e
MacRat <m@crat.jp>
2018-02-11 20:37:41 +0900

    Made examples

---
+++ examples/simplest.js
@@ -0,0 +1,8 @@
+const PromiseBench = require('../dist/promise-bench.js');
+
+
+new PromiseBench.Benchmark(function() {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, 100)
+	});
+}).run().catch(console.error);
