0308b07d4b417551d4cbf3c25f3c54d1b24c203b
MacRat <m@crat.jp>
2018-02-12 21:27:42 +0900

    Added eslint into build.js

--- build.js
+++ build.js
@@ -3,13 +3,13 @@ const babel = require('rollup-plugin-babel');
 
 
 rollup
-	.rollup({
-		input: 'src/index.js',
-		plugins: [babel()],
-	})
-	.then(bundle => Promise.all([
-		bundle.write({format: 'es',  exports: 'named', file: 'dist/promise-bench.mjs'}),
-		bundle.write({format: 'cjs', exports: 'named', file: 'dist/promise-bench.js'}),
-		bundle.write({format: 'umd', exports: 'named', file: 'dist/promise-bench.web.js', name: 'PromiseBench'}),
-	]))
-	.catch(console.error);
+    .rollup({
+        input: 'src/index.js',
+        plugins: [babel()],
+    })
+    .then(bundle => Promise.all([
+        bundle.write({format: 'es',  exports: 'named', file: 'dist/promise-bench.mjs'}),
+        bundle.write({format: 'cjs', exports: 'named', file: 'dist/promise-bench.js'}),
+        bundle.write({format: 'umd', exports: 'named', file: 'dist/promise-bench.web.js', name: 'PromiseBench'}),
+    ]))
+    .catch(console.error);
