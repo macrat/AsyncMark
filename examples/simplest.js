0068ca6e2b803a7411a25c5d371edd97fa329155
MacRat <m@crat.jp>
2020-07-04 18:10:16 +0900

    Fixed lint error

--- examples/simplest.js
+++ examples/simplest.js
@@ -2,7 +2,7 @@ const AsyncMark = require('../dist/asyncmark.js');
 
 
 new AsyncMark.Benchmark(function() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(resolve, 100);
     });
 }).run().catch(console.error);
