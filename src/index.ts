23a8504990cd9a9e0c462c877089462c29594a1d
MacRat <m@crat.jp>
2020-07-05 16:45:41 +0900

    Added exports types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 export { default as Result } from './result';
 
-export { default as Benchmark } from './benchmark';
+export { default as Benchmark, BenchmarkOptions } from './benchmark';
 export { default } from './benchmark';
 
-export { default as Suite } from './suite';
+export { default as Suite, SuiteOptions } from './suite';
+
+export { TargetFunc, TestCallbacks } from './callbacks';
+
+export { Operator } from './assertion';
 
 export { timeit } from './timer';
